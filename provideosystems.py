b40db549a489e00304873e77eeaa0c5fa473cf36
DocenkoG <docn@mail.ru>
2019-02-18 23:19:09 +0300

    Переделал на Selenium

--- provideosystems.py
+++ provideosystems.py
@@ -153,6 +153,11 @@ def convert_excel2csv(cfg):
 
 
 def download( cfg ):
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.remote.remote_connection import LOGGER
+    LOGGER.setLevel(logging.WARNING)
+     
     retCode     = False
     filename_new= cfg.get('download','filename_new')
     filename_old= cfg.get('download','filename_old')
@@ -160,53 +165,162 @@ def download( cfg ):
     password    = cfg.get('download','password' )
     url_lk      = cfg.get('download','url_lk'   )
     url_file    = cfg.get('download','url_file' )
-    headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.0; rv:14.0) Gecko/20100101 Firefox/14.0.1',
-               'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-               'Accept-Language':'ru-ru,ru;q=0.8,en-us;q=0.5,en;q=0.3',
-               'Accept-Encoding':'gzip, deflate',
-               'Connection':'keep-alive',
-               'DNT':'1'
-              }    
-    try:
-        s = requests.Session()
-        r = s.get(url_lk,  headers = headers) 
-        #print(r.text)                         # посмотреть исходный код страницы
-        page = lxml.html.fromstring(r.text)
-        for ff in page.forms:                 # посмотреть список форм и их поля
-            print(ff.fields.keys())
-        form = page.forms[2]
-        form.fields['login'] = login
-        form.fields['pass'] = password
-        r = s.post(url_lk+ form.action, data=form.form_values())
-        print('       ==================================================')
-        #print('<<<',r.text,'>>>')
 
-        log.debug('Авторизация на %s   --- code=%d', url_lk, r.status_code)
-        r = s.get(url_file)
-        log.debug('Загрузка файла %16d bytes   --- code=%d', len(r.content), r.status_code)
-        retCode = True
-        '''
-        s = requests.Session()
-        r = s.get(url_lk, auth=(login,password))  # ,headers = headers (И без него сработало, но где-то может понадобиться)
-        page = lxml.html.fromstring(r.text)
-        # data = {'USER_LOGIN':login, 'USER_PASSWORD':password})
-        log.debug('Авторизация на %s   --- code=%d', url_lk, r.status_code)
-        r = s.get(url_file)
-        log.debug('Загрузка файла %24d bytes   --- code=%d', len(r.content), r.status_code)
-        retCode = True
-        '''
+    download_path= os.path.join(os.getcwd(), 'tmp')
+    if not os.path.exists(download_path):
+        os.mkdir(download_path)
+
+    for fName in os.listdir(download_path) :
+        os.remove( os.path.join(download_path, fName))
+    dir_befo_download = set(os.listdir(download_path))
+        
+    if os.path.exists('geckodriver.log') : os.remove('geckodriver.log')
+    try:
+        ffprofile = webdriver.FirefoxProfile()
+        ffprofile.set_preference("browser.download.dir", download_path)
+        ffprofile.set_preference("browser.download.folderList",2);
+        ffprofile.set_preference("browser.helperApps.neverAsk.saveToDisk", 
+                ",application/octet-stream" + 
+                ",application/vnd.ms-excel" + 
+                ",application/vnd.msexcel" + 
+                ",application/x-excel" + 
+                ",application/x-msexcel" + 
+                ",application/zip" + 
+                ",application/xls" + 
+                ",application/vnd.ms-excel" +
+                ",application/vnd.ms-excel.addin.macroenabled.12" +
+                ",application/vnd.ms-excel.sheet.macroenabled.12" +
+                ",application/vnd.ms-excel.template.macroenabled.12" +
+                ",application/vnd.ms-excelsheet.binary.macroenabled.12" +
+                ",application/vnd.ms-fontobject" +
+                ",application/vnd.ms-htmlhelp" +
+                ",application/vnd.ms-ims" +
+                ",application/vnd.ms-lrm" +
+                ",application/vnd.ms-officetheme" +
+                ",application/vnd.ms-pki.seccat" +
+                ",application/vnd.ms-pki.stl" +
+                ",application/vnd.ms-word.document.macroenabled.12" +
+                ",application/vnd.ms-word.template.macroenabed.12" +
+                ",application/vnd.ms-works" +
+                ",application/vnd.ms-wpl" +
+                ",application/vnd.ms-xpsdocument" +
+                ",application/vnd.openofficeorg.extension" +
+                ",application/vnd.openxmformats-officedocument.wordprocessingml.document" +
+                ",application/vnd.openxmlformats-officedocument.presentationml.presentation" +
+                ",application/vnd.openxmlformats-officedocument.presentationml.slide" +
+                ",application/vnd.openxmlformats-officedocument.presentationml.slideshw" +
+                ",application/vnd.openxmlformats-officedocument.presentationml.template" +
+                ",application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" +
+                ",application/vnd.openxmlformats-officedocument.spreadsheetml.template" +
+                ",application/vnd.openxmlformats-officedocument.wordprocessingml.template" +
+                ",application/x-ms-application" +
+                ",application/x-ms-wmd" +
+                ",application/x-ms-wmz" +
+                ",application/x-ms-xbap" +
+                ",application/x-msaccess" +
+                ",application/x-msbinder" +
+                ",application/x-mscardfile" +
+                ",application/x-msclip" +
+                ",application/x-msdownload" +
+                ",application/x-msmediaview" +
+                ",application/x-msmetafile" +
+                ",application/x-mspublisher" +
+                ",application/x-msschedule" +
+                ",application/x-msterminal" +
+                ",application/x-mswrite" +
+                ",application/xml" +
+                ",application/xml-dtd" +
+                ",application/xop+xml" +
+                ",application/xslt+xml" +
+                ",application/xspf+xml" +
+                ",application/xv+xml" +
+                ",application/excel")
+        if os.name == 'posix':
+            #driver = webdriver.Firefox(ffprofile, executable_path=r'/usr/local/Cellar/geckodriver/0.19.1/bin/geckodriver')
+            driver = webdriver.Firefox(ffprofile, executable_path=r'/usr/local/bin/geckodriver')
+        elif os.name == 'nt':
+            driver = webdriver.Firefox(ffprofile)
+        driver.implicitly_wait(10)
+        
+        driver.get(url_lk)
+        time.sleep(2)
+        driver.find_element_by_name("login").click()
+        driver.find_element_by_name("login").clear()
+        driver.find_element_by_name("login").send_keys(login)
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        driver.find_element_by_id("partners_auth_btn").click()
+        time.sleep(1)
+        driver.get(url_file)
+        time.sleep(5)
+        #driver.close()
+        driver.find_element_by_link_text(u"Выход").click()        
+        driver.quit()
+
     except Exception as e:
         log.debug('Exception: <' + str(e) + '>')
 
-    if os.path.exists( filename_new) and os.path.exists( filename_old): 
-        os.remove( filename_old)
-        os.rename( filename_new, filename_old)
-    if os.path.exists( filename_new) :
-        os.rename( filename_new, filename_old)
-    f2 = open(filename_new, 'wb')                                  #Теперь записываем файл
-    f2.write(r.content)
-    f2.close()
+    dir_afte_download = set(os.listdir(download_path))
+    new_files = list( dir_afte_download.difference(dir_befo_download))
+    print(new_files)
+    if len(new_files) == 0 :        
+        log.error( 'Не удалось скачать файл прайса ')
+        retCode= False
+    elif len(new_files)>1 :
+        log.error( 'Скачалось несколько файлов. Надо разбираться ...')
+        retCode= False
+    else:   
+        new_file = new_files[0]                                                     # загружен ровно один файл. 
+        new_ext  = os.path.splitext(new_file)[-1].lower()
+        DnewFile = os.path.join( download_path,new_file)
+        new_file_date = os.path.getmtime(DnewFile)
+        log.info( 'Скачанный файл ' +new_file + ' имеет дату ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(new_file_date) ) )
+        
+        print(new_ext)
+        if new_ext in ('.xls','.xlsx','.xlsb','.xlsm','.csv'):
+            if os.path.exists( filename_new) and os.path.exists( filename_old): 
+                os.remove( filename_old)
+                os.rename( filename_new, filename_old)
+            if os.path.exists( filename_new) :
+                os.rename( filename_new, filename_old)
+            shutil.copy2( DnewFile, filename_new)
+            retCode= True
+
+        elif new_ext == '.zip':  
+            # ветка устаревшая, не проверялась                                      # Архив. Обработка не завершена
+            log.debug( 'Zip-архив. Разархивируем.')
+            work_dir = os.getcwd()                                                  
+            os.chdir( os.path.join( download_path ))
+            dir_befo_download = set(os.listdir(os.getcwd()))
+            os.system('unzip -oj ' + new_file)
+            os.remove(new_file)   
+            dir_afte_download = set(os.listdir(os.getcwd()))
+            new_files = list( dir_afte_download.difference(dir_befo_download))
+            os.chdir(work_dir)
+            if len(new_files) == 1 :   
+                new_file = new_files[0]                                             # разархивирован ровно один файл. 
+                new_ext  = os.path.splitext(new_file)[-1]
+                DnewFile = os.path.join( download_path,new_file)
+                new_file_date = os.path.getmtime(DnewFile)
+                log.debug( 'Файл из архива ' +DnewFile + ' имеет дату ' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(new_file_date) )     )
+                filename_in= cfg.get('basic','filename_in')
+                if os.path.exists( filename_new) and os.path.exists( filename_old): 
+                    os.remove( filename_old)
+                    os.rename( filename_new, filename_old)
+                if os.path.exists( filename_new) :
+                    os.rename( filename_new, filename_old)
+                shutil.copy2( DnewFile, filename_new)
+                retCode=True
+
+            elif len(new_files) >1 :
+                log.debug( 'В архиве не единственный файл. Надо разбираться.')
+                retCode=False
+            else:
+                log.debug( 'Нет новых файлов после разархивации. Загляни в папку юниттеста поставщика.')
+                retCode=False
     return retCode
+
 
 
 
